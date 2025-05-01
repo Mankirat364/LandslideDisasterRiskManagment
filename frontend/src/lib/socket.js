7953c90458f1f335b3c3fc9063ef956c44829385
Mankirat364 <mankirat.techguild@gmail.com>
2025-05-01 13:52:32 +0530

    Update socket.js

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -5,7 +5,10 @@ let socket;
 
 export const initSocket = (baseUrl) => {
   if (!socket) {
-    socket = io(baseUrl);
+    socket = io(baseUrl , {
+       withCredentials: true,
+  transports: ['websocket']
+    });
   }
   return socket;
 };
