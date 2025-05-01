51386fd8c62047c563b188e94a0b71fc6391910b
Mankirat364 <mankirat.techguild@gmail.com>
2025-05-01 13:42:29 +0530

    Update socket.js

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -7,7 +7,7 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: 'http://localhost:5173',
+      origin: 'https://landslidedisasterriskmanagmentfrontend.onrender.com',
       credentials: true
     }
   });
