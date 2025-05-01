8552657032f4b0a218879e81cc60325e63e2e2db
Mankirat364 <mankirat.techguild@gmail.com>
2025-05-01 13:36:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ initSocket(server)
 const PORT = process.env.PORT || 3001
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://landslidedisasterriskmanagmentfrontend.onrender.com'],
   credentials: true
 }))
 app.use(express.json())
